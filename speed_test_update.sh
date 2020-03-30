2838d4f9eee924b4cfbc89a1bccb48b8df0dc3f7
Jeff Stander <jrusty15@gmail.com>
2020-03-29 20:10:39 -0500

    Added speed test script

---
+++ speed_test_update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+DATA="$(speedtest-cli --bytes --csv --csv-delimiter ';')"
+PING=""
+DOWNLOAD=""
+UPLOAD=""
+IP=""
+
+#7 = ping
+#8 = download speed in bytes
+#9 = upload speed in bytes
+#10 = public IP address
+
+arrData=(${DATA//;/ })
+
+
+PING=${arrData[7]}
+DOWNLOAD=${arrData[8]}
+UPLOAD=${arrData[9]}
+IP=${arrData[10]}
+
+curl -L -X POST 'http://192.168.1.109:8086/write?db=extmonitors' \
+        -H 'Content-Type: text/plain' \
+        --data-raw "isp,data_type=ping value=${PING}"
+
+curl -L -X POST 'http://192.168.1.109:8086/write?db=extmonitors' \
+        -H 'Content-Type: text/plain' \
+        --data-raw "isp,data_type=download value=${DOWNLOAD}"
+
+curl -L -X POST 'http://192.168.1.109:8086/write?db=extmonitors' \
+        -H 'Content-Type: text/plain' \
+        --data-raw "isp,data_type=upload value=${UPLOAD}"
+
+
